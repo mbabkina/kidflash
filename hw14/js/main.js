1a1a1793c32efd8e76fd1736a21207f05da52f6e
maryna.babkina <babckina@gmail.com>
2020-10-25 14:50:21 +0200

    hw14 commit

--- hw14/js/main.js
+++ hw14/js/main.js
@@ -1,3 +1,9 @@
-/* 'use strict';
-
-console.log('test'); */
+$(document).ready(function(){
+      $('.review-slider').slick({
+          arrows: true,
+          prevArrow: ".arrow-left",
+          nextArrow: ".arrow-right"
+          
+      });
+});
+$('.single-item').slick();s
